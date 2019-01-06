e05b5b84efba0ecd6ad6f438b32d178446c86538
Sebastian <anse23@hotmail.com>
2019-01-05 20:23:03 -0500

    test: Added test for CellCont
    The tests verify the props correctly passed to
    6 different types of cells.

--- src/js/containers/CellCont.test.js
+++ src/js/containers/CellCont.test.js
@@ -1,36 +1,117 @@
 const React = require('react');
 const { shallow, mount } = require('enzyme');
 const { Provider } = require('react-redux');
+const configureStore = require('redux-mock-store').default;
 const { CellComp } = require('../components/CellComp.js');
 const { CellCont } = require('./CellCont.js')
-const { storeFake, initialState } = require('../../test/utilsTests.js');
+const { moveToken, updateScores } = require('../actions.js');
+const { GAME_STATUSES } = require('../model/constants.js');
+const {
+    initialState,
+    verticalPosition,
+    horizontalPosition,
+    otherPosition,
+    verticalPositionHid,
+    horizontalPositionHid,
+    otherPositionHid
+} = require('../../test/utilsTests.js');
+
+const middlewares = [];
+const mockStore = configureStore(middlewares);
 
 describe("CellCont", () => {
-    let wrapper;
-    let component;
-    let container;
+    let wrapper, store;
+    let containerVertical, containerHorizontal, containerOther;
+    let componentVertical, componentHorizontal, componentOther;
+    let containerVerticalHid, containerHorizontalHid, containerOtherHid;
+    let componentVerticalHid, componentHorizontalHid, componentOtherHid;
 
     beforeEach(() => {
         jest.resetAllMocks();
 
-        const store = storeFake(initialState);
+        store = mockStore(initialState);
+        store.dispatch = jest.fn();
 
         wrapper = mount(
             <Provider store={store}>
-                <CellCont />
+                <CellCont
+                    rowIndex={verticalPosition.rowIndex}
+                    colIndex={verticalPosition.colIndex}
+                    value={verticalPosition.value}/>
+                <CellCont
+                    rowIndex={horizontalPosition.rowIndex}
+                    colIndex={horizontalPosition.colIndex}
+                    value={horizontalPosition.value}/>
+                <CellCont
+                    rowIndex={otherPosition.rowIndex}
+                    colIndex={otherPosition.colIndex}
+                    value={otherPosition.value}/>
+                <CellCont
+                    rowIndex={verticalPositionHid.rowIndex}
+                    colIndex={verticalPositionHid.colIndex}
+                    value={verticalPositionHid.value}/>
+                <CellCont
+                    rowIndex={horizontalPositionHid.rowIndex}
+                    colIndex={horizontalPositionHid.colIndex}
+                    value={horizontalPositionHid.value}/>
+                <CellCont
+                    rowIndex={otherPositionHid.rowIndex}
+                    colIndex={otherPositionHid.colIndex}
+                    value={otherPositionHid.value}/>
             </Provider>
         );
 
-        container = wrapper.find(CellCont);
-        component = container.find(CellComp);
+        containerVertical = wrapper.find(CellCont).at(0);
+        containerHorizontal = wrapper.find(CellCont).at(1);
+        containerOther = wrapper.find(CellCont).at(2);
+        containerVerticalHid = wrapper.find(CellCont).at(3);
+        containerHorizontalHid = wrapper.find(CellCont).at(4);
+        containerOtherHid = wrapper.find(CellCont).at(5);
+
+        componentVertical = containerVertical.find(CellComp).at(0);
+        componentHorizontal = containerHorizontal.find(CellComp).at(0);
+        componentOther = containerOther.find(CellComp).at(0);
+        componentVerticalHid = containerVerticalHid.find(CellComp).at(0);
+        componentHorizontalHid = containerHorizontalHid.find(CellComp).at(0);
+        componentOtherHid = containerOtherHid.find(CellComp).at(0);
     });
 
     it("should render container and component", () => {
-        expect(container.length).toBeTruthy();
-        expect(component.length).toBeTruthy();
+        expect(containerVertical.length).toBeTruthy();
+        expect(componentVertical.length).toBeTruthy();
+
+        expect(containerHorizontal.length).toBeTruthy();
+        expect(componentHorizontal.length).toBeTruthy();
+
+        expect(containerOther.length).toBeTruthy();
+        expect(componentOther.length).toBeTruthy();
+
+        expect(containerVerticalHid.length).toBeTruthy();
+        expect(componentVerticalHid.length).toBeTruthy();
+
+        expect(containerHorizontalHid.length).toBeTruthy();
+        expect(componentHorizontalHid.length).toBeTruthy();
+
+        expect(containerOtherHid.length).toBeTruthy();
+        expect(componentOtherHid.length).toBeTruthy();
     });
 
-    it("should map to props", () => {
+    it("should map to container props", () => {
+        const expectedPropKeys = [
+            'rowIndex',
+            'colIndex',
+            'value'
+        ];
+        expect(Object.keys(containerVertical.props())).toEqual(expect.arrayContaining(expectedPropKeys));
+        expect(Object.keys(containerHorizontal.props())).toEqual(expect.arrayContaining(expectedPropKeys));
+        expect(Object.keys(containerOther.props())).toEqual(expect.arrayContaining(expectedPropKeys));
+
+        expect(Object.keys(containerVerticalHid.props())).toEqual(expect.arrayContaining(expectedPropKeys));
+        expect(Object.keys(containerHorizontalHid.props())).toEqual(expect.arrayContaining(expectedPropKeys));
+        expect(Object.keys(containerOtherHid.props())).toEqual(expect.arrayContaining(expectedPropKeys));
+    });
+
+    it("should map to component props", () => {
         const expectedPropKeys = [
             'rowIndex',
             'colIndex',
@@ -40,7 +121,56 @@ describe("CellCont", () => {
             'value',
             'onClick'
         ];
+        expect(Object.keys(componentVertical.props())).toEqual(expect.arrayContaining(expectedPropKeys));
+        expect(Object.keys(componentHorizontal.props())).toEqual(expect.arrayContaining(expectedPropKeys));
+        expect(Object.keys(componentOther.props())).toEqual(expect.arrayContaining(expectedPropKeys));
 
-        expect(Object.keys(component.props())).toEqual(expect.arrayContaining(expectedPropKeys));
+        expect(Object.keys(componentVerticalHid.props())).toEqual(expect.arrayContaining(expectedPropKeys));
+        expect(Object.keys(componentHorizontalHid.props())).toEqual(expect.arrayContaining(expectedPropKeys));
+        expect(Object.keys(componentOtherHid.props())).toEqual(expect.arrayContaining(expectedPropKeys));
+    });
+
+    it("should have props with correct values", () => {
+        expect(componentVertical.props().rowIndex).toEqual(verticalPosition.rowIndex);
+        expect(componentVertical.props().colIndex).toEqual(verticalPosition.colIndex);
+        expect(componentVertical.props().style).toEqual('cell selectable');
+        expect(componentVertical.props().isSelectable).toEqual(true);
+        expect(componentVertical.props().gameStatus).toEqual(GAME_STATUSES.RESTING);
+        expect(componentVertical.props().value).toEqual(verticalPosition.value);
+
+        expect(componentHorizontal.props().rowIndex).toEqual(horizontalPosition.rowIndex);
+        expect(componentHorizontal.props().colIndex).toEqual(horizontalPosition.colIndex);
+        expect(componentHorizontal.props().style).toEqual('cell selectable');
+        expect(componentHorizontal.props().isSelectable).toEqual(true);
+        expect(componentHorizontal.props().gameStatus).toEqual(GAME_STATUSES.RESTING);
+        expect(componentHorizontal.props().value).toEqual(horizontalPosition.value);
+
+        expect(componentOther.props().rowIndex).toEqual(otherPosition.rowIndex);
+        expect(componentOther.props().colIndex).toEqual(otherPosition.colIndex);
+        expect(componentOther.props().style).toEqual('cell');
+        expect(componentOther.props().isSelectable).toEqual(false);
+        expect(componentOther.props().gameStatus).toEqual(GAME_STATUSES.RESTING);
+        expect(componentOther.props().value).toEqual(otherPosition.value);
+
+        expect(componentVerticalHid.props().rowIndex).toEqual(verticalPositionHid.rowIndex);
+        expect(componentVerticalHid.props().colIndex).toEqual(verticalPositionHid.colIndex);
+        expect(componentVerticalHid.props().style).toEqual('cell hid');
+        expect(componentVerticalHid.props().isSelectable).toEqual(false);
+        expect(componentVerticalHid.props().gameStatus).toEqual(GAME_STATUSES.RESTING);
+        expect(componentVerticalHid.props().value).toEqual(verticalPositionHid.value);
+
+        expect(componentHorizontalHid.props().rowIndex).toEqual(horizontalPositionHid.rowIndex);
+        expect(componentHorizontalHid.props().colIndex).toEqual(horizontalPositionHid.colIndex);
+        expect(componentHorizontalHid.props().style).toEqual('cell hid');
+        expect(componentHorizontalHid.props().isSelectable).toEqual(false);
+        expect(componentHorizontalHid.props().gameStatus).toEqual(GAME_STATUSES.RESTING);
+        expect(componentHorizontalHid.props().value).toEqual(horizontalPositionHid.value);
+
+        expect(componentOtherHid.props().rowIndex).toEqual(otherPositionHid.rowIndex);
+        expect(componentOtherHid.props().colIndex).toEqual(otherPositionHid.colIndex);
+        expect(componentOtherHid.props().style).toEqual('cell hid');
+        expect(componentOtherHid.props().isSelectable).toEqual(false);
+        expect(componentOtherHid.props().gameStatus).toEqual(GAME_STATUSES.RESTING);
+        expect(componentOtherHid.props().value).toEqual(otherPositionHid.value);
     });
 });
--- src/test/utilsTests.js
+++ src/test/utilsTests.js
@@ -1,14 +1,5 @@
 const { PLAYER_DIRECTIONS, TURNS, GAME_STATUSES } = require('../js/model/constants.js');
 
-const storeFake = (state) => {
-    return {
-        default: jest.fn(),
-        subscribe: jest.fn(),
-        dispatch: jest.fn(),
-        getState: () => state
-    };
-};
-
 const initialState = {
     token: {
         rowIndex: 3,
@@ -18,9 +9,9 @@ const initialState = {
     },
     board: [
         2, 3, 4, 7, 8,
-        1, 1, 3, 5, 6,
+        1, -1, 3, -1, 6,
         2, 7, 6, 4, 1,
-        5, 2, 9, 0, 2,
+        5, -1, 9, 0, 2,
         8, 7, 9, 2, 1
     ],
     player1: {
@@ -39,7 +30,48 @@ const initialState = {
     }
 };
 
+const verticalPosition = {
+    rowIndex: 0,
+    colIndex: 3,
+    value: 7
+};
+
+const horizontalPosition = {
+    rowIndex: 3,
+    colIndex: 0,
+    value: 5
+};
+
+const otherPosition = {
+    rowIndex: 2,
+    colIndex: 2,
+    value: 6
+};
+
+const verticalPositionHid = {
+    rowIndex: 1,
+    colIndex: 3,
+    value: -1
+};
+
+const horizontalPositionHid = {
+    rowIndex: 3,
+    colIndex: 1,
+    value: -1
+};
+
+const otherPositionHid = {
+    rowIndex: 1,
+    colIndex: 1,
+    value: -1
+};
+
 module.exports = {
-    storeFake,
-    initialState
+    initialState,
+    verticalPosition,
+    horizontalPosition,
+    otherPosition,
+    verticalPositionHid,
+    horizontalPositionHid,
+    otherPositionHid
 };
