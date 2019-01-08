86a73daefcf3efe31c6ada06ff0367776f136f0c
Sebastian <anse23@hotmail.com>
2019-01-08 13:07:54 -0500

    feat: Reimplemented the local storage feature in the reducer

--- src/js/reducer.js
+++ src/js/reducer.js
@@ -1,33 +1,60 @@
 const { Game } = require('./model/Game.js');
-const { GAME_STATUSES } = require('./model/constants.js');
+const { LocalStorageManager } = require('./data/LocalStorageManager.js');
+const { GAME_STATUSES, TURNS } = require('./model/constants.js');
 const { updateObjectFromLiteral } = require('./model/utils.js');
 const types = require('./actionTypes.js');
 
-const boardSize = 5;
+const boardSize = 9;
 const game = new Game(boardSize);
+
+const localStorageManager = new LocalStorageManager();
+const previousState = localStorageManager.getGameState();
+if (previousState !== null) {
+    game.updateFromObject(previousState);
+} else {
+    localStorageManager.setGameState(game.serialize());
+}
 const initialState = game.serialize();
+
+const doMoveToken = (state, action) => {
+    game.updateFromObject(state);
+    game.moveToken(action.rowIndex, action.colIndex);
+    game.takeCell();
+    game.setStatus(GAME_STATUSES.MOVING_TOKEN);
+    return game.serialize();
+};
+
+const doUpdateScores = state => {
+    game.updateFromObject(state);
+    game.updateScores();
+    game.updateBoard();
+    game.passToken();
+    game.setStatus(GAME_STATUSES.RESTING);
+    game.updateContinuity();
+    const serialized = game.serialize();
+    if (game.snap.turn === TURNS.PLAYER1) { // Save when player is human
+        localStorageManager.setGameState(serialized)
+    }
+    return serialized;
+};
+
+const doResetGame = (state, action) => {
+    const newGame = new Game(action.boardSize);
+    const serialized = newGame.serialize();
+    localStorageManager.setGameState(serialized);
+    return serialized;
+};
 
 const reduce = (state = initialState, action) => {
     switch (action.type) {
         case types.TOKEN_MOVED:
-            game.updateFromObject(state);
-            game.moveToken(action.rowIndex, action.colIndex);
-            game.takeCell();
-            game.setStatus(GAME_STATUSES.MOVING_TOKEN);
-            return game.serialize();
+            return doMoveToken(state, action);
 
         case types.SCORES_UPDATED:
-            game.updateFromObject(state);
-            game.updateScores();
-            game.updateBoard();
-            game.passToken();
-            game.setStatus(GAME_STATUSES.RESTING);
-            game.updateContinuity();
-            return game.serialize();
+            return doUpdateScores(state);
 
         case types.GAME_RESET:
-            const newGame = new Game(action.boardSize);
-            return newGame.serialize();
+            return doResetGame(state, action);
 
         default:
             return state;
