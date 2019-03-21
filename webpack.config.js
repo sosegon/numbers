a1d530fc17dbd0f623ee604bac88676ca2dcdaa8
Sebastian <anse23@hotmail.com>
2019-03-21 15:06:50 -0500

    conf: Added dependency and set webpack to copy images

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 	entry: {
@@ -43,6 +44,9 @@ module.exports = {
 		}),
 		new MiniCssExtractPlugin({
 			filename: '../css/style.css'
-		})
+		}),
+		new CopyWebpackPlugin([
+			{from: './src/images', to: '../images'}
+		])
 	]
 };
